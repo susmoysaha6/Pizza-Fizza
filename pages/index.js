dd5d6dbdaf433f83e6ad2ac9a373929d4ea0a600
susmoysaha6 <susmoysaha6@gmail.com>
2022-12-31 17:53:58 +0600

    pizza list and footer added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Featured from '../components/Featured'
+import PizzaList from '../components/PizzaList'
 
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       < Featured />
+      <PizzaList />
     </div>
   )
 }
